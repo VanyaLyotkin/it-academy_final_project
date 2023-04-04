8ddc46a2710eb0b68ca62373945e1a60ca4606df
flashback <freezeeeeen@gmail.com>
2023-04-04 16:19:35 +0300

    add query sliders

--- src/js/script.js
+++ src/js/script.js
@@ -1,45 +1,21 @@
-var button = document.querySelectorAll('.advantages-list__btn');
-var text = document.querySelectorAll('.advantages-list__text');
-
-[].forEach.call(button, function(btn, i) {
-    btn.addEventListener('click', function() {
-        text[i].classList.toggle('show-text');
+$(function() {
+    $('.advantages-list__btn').on('click', function() {
+        var block = $(this).closest('.advantages-list__item');
+        block.find('.advantages-list__text').slideToggle(400).toggleClass('show-text');
     });
 });
 
-// document.querySelector('.advantages-list__btn').onclick = function() {
-//     var move = document.querySelector('advantages-list__text').classList.toggle("move");
-// };
-
-var $foo = $('.foo');
-
-$foo
-    .addClass('block')
-    .outerWidth(); // Reflow
-
-$foo
-    .addClass('fade-in')
-    .one(transitionEnd, function() {
-        alert('Animated');
+$( document ).ready(function() {
+  $("#show-gallery").click(function() {
+    if ($("#invisible-gallery").css('display') === 'none') {
+        $("#invisible-gallery").slideDown({
+            start: function() {
+              $(this).css('display', 'grid');
+            }
+        });
+    }
+    else {
+        $("#invisible-gallery").slideUp(600);
+    }
     });
-
-// $(document).ready(function(){
-//     $('.advantages-list__btn').click(function(){
-//       $(".advantages-list__text").fadeOut(600);
-//     });
-//     $('.advantages-list__btn').click(function(){
-//       $(".advantages-list__text").fadeIn(600);
-//     });
-// });
-
-// const btn = document.querySelector('.advantages-list__btn');
-// btn.addEventListener('click', _ => document.querySelector('.advantages-list__text').classList.toggle('move'));
-
-//     if(advantagesListText.style.display === 'none') {
-//         advantagesListText.style.display = 'block';
-//         advantagesListBtn.style.background = '#000000';
-//     } else {
-//         advantagesListText.style.display = 'none';
-//         advantagesListBtn.style.background = '#777777';
-//     }
-// }
+});
