152bcc93e1f5436ed9126f0b0f4a6a67f95b4345
flashback <freezeeeeen@gmail.com>
2023-04-16 15:53:15 +0300

    add adaptive

--- src/js/script.js
+++ src/js/script.js
@@ -95,24 +95,33 @@ $(document).ready(function() {
     arrows: true,
     responsive: [
         {
-            breakpoint: 1024,
+            breakpoint: 1025,
             settings: {
                 slidesToShow: 3,
-                slidesToScroll: 2,
+                slidesToScroll: 3,
                 infinite: true,
                 dots: true
             }
         },
         {
-            breakpoint: 600,
+            breakpoint: 768,
             settings: {
                 slidesToShow: 2,
-                slidesToScroll: 2
+                slidesToScroll: 2,
+                arrows: false
             }
         },
         {
             breakpoint: 480,
             settings: {
+                
+                slidesToScroll: 1
+            }
+        },
+        {   
+            breakpoint: 320,
+            settings: {
+                arrows: false,
                 slidesToShow: 1,
                 slidesToScroll: 1
             }
